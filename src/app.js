bd6baae041f220912334f10eadcf723c39f0fd4a
mnstallworth09 <michellestallworth@ymail.com>
2023-03-10 11:46:17 -0800

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let descritpionElement = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.main.wind.speed);
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "to3a410424f8a76d0f4c38679e84eb28";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=San%20Francisco&appid=${apikey}&units=metric";
-axios.get(url).then(displayTemperature);
+let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=San%20Francisco&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
